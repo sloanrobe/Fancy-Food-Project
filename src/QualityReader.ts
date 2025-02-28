145e30a58dfd26d66b79979b583372550b2207e7
Benji Sloan <sloanrobe@hotmail.com>
2025-02-28 16:54:30 +0100

    Added supplier class and functionality

---
+++ src/QualityReader.ts
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import { Supplier } from './Supplier'
+
+export class QualityReader {
+    filePath = './data/supplierQuality.csv'
+
+    async main() {
+        return await this.readFile()
+    }
+
+    async readFile() {
+        const suppliers: Supplier[] = []
+        fs.readFile(this.filePath, 'utf-8', (err, fileContent) => {
+            if (err) {
+                console.log(err)
+            } else {
+                const lines = fileContent.split('\r\n')
+                for (let i = 0; i < lines.length; i++) {
+                    const productString = lines[i];  
+                    const values = productString.split(',')
+                    const priceSplit = values[3].split('$')
+                    const product = new Supplier(values[0], values[1])
+                    suppliers.push(product)
+                }
+            }
+        }) 
+        return suppliers  
+    }
+}
---
+++ src/Supplier.ts
@@ -0,0 +1,9 @@
+export class Supplier {
+    supplier: string;
+    quality_score: string;
+
+    constructor(supplier: string, quality_score: string) {
+        this.supplier = supplier,
+        this.quality_score = quality_score
+    }
+}
