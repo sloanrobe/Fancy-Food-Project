c6ad5197dc222a1ff3f2d94adfde11ee897efc4d
Benji Sloan <sloanrobe@hotmail.com>
2025-03-04 10:58:21 +0100

    Minor formatting

--- src/InventoryReader.ts
+++ src/InventoryReader.ts
@@ -28,6 +28,40 @@ export class InventoryReader {
     }
 } */
 
+import fs from 'fs'
+import { FoodItem } from './FoodItem'
+
+export class InventoryReader {
+    filePath = './data/inventory.csv'
+
+    async main() {
+        return await this.readFile()
+    }
+
+    async readFile() {
+        return new Promise<FoodItem[]>((resolve, reject) => {
+            fs.readFile(this.filePath, 'utf-8', (err, fileContent) => {
+                const foodItems: FoodItem[] = []
+                if (err) {
+                    console.log(err)
+                    reject(err)
+                } else {
+                    const lines = fileContent.split('\r\n')
+                    for (let i = 0; i < lines.length; i++) {
+                        const productString = lines[i];  
+                        const values = productString.split(',')
+                        const priceSplit = values[3].split('$')
+                        const product = new FoodItem(values[0], values[1], values[2], Number(priceSplit[1]), values[4])
+                        foodItems.push(product)
+                    }
+                }
+                resolve(foodItems)
+            })
+        })
+    }
+}
+
+/* 
     import fs from 'fs/promises'
     import { FoodItem } from './FoodItem'
     
@@ -55,4 +89,4 @@ export class InventoryReader {
                 return []
             }
         }
-    }
+    } */
