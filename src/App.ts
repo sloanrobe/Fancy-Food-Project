ee24e786890a0a80dc7a30c5050e64b7623a6417
Benji Sloan <sloanrobe@hotmail.com>
2025-03-04 10:58:00 +0100

    Added runQuality function

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,18 @@
 import { FoodItem } from "./FoodItem"
 import { InventoryReader } from "./InventoryReader"
+import { QualityReader } from "./QualityReader"
 
-async function run() {
+async function runInventory() {
     const inventoryReader = new InventoryReader()
     const actualProducts = await inventoryReader.main()
     console.log(actualProducts)
 }
 
-run()
+async function runQuality() {
+    const qualityReader = new QualityReader()
+    const actualQuality = await qualityReader.main()
+    console.log(actualQuality)
+}
+
+runInventory()
+runQuality()
