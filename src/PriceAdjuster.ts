2e7c2be6f4663af8f5b8096f54fb75decd5124de
Benji Sloan <sloanrobe@hotmail.com>
2025-03-14 13:30:21 +0100

    Updated logic for food categories

--- src/PriceAdjuster.ts
+++ src/PriceAdjuster.ts
@@ -10,23 +10,23 @@ export class PriceAdjuster {
     }
     
     checkPrice(foodItems: FoodItem[]) {
+        const today = new Date('2024-06-10')
+        
         for (let i = 0; i < foodItems.length; i++) {
             const foodItem = foodItems[i];
 
             if (foodItem.category === 'Dairy' && this.shouldUpdatePrice(foodItem.expiry_date, new Date('2024-06-10'), 4)) {
-                foodItem.price. reduce by 50%
-            } else if (foodItem.category === 'Canned Goods') {
-    
-            } else if (foodItem.category === 'Vegetables') {
-
-            }
-            
+                foodItem.price *= 0.5
+            } else if (foodItem.category === 'Vegetables' && this.shouldUpdatePrice(foodItem.expiry_date, new Date('2024-06-10'), 3)) {
+                foodItem.price *= 0.6
+            } else (foodItem.category === 'Canned Goods') {
+                const expiryDate = new Date(foodItem.expiry_date)
+                const threeMonthsFromNow = new Date(today)
+                threeMonthsFromNow.setMonth(today.getMonth() + 3)
+                if (expiryDate < threeMonthsFromNow) {
+                    foodItem.price *= 0.75
+                } 
+            }  
         }
     }
 }
-
-//This is the strategy for price reduction: For Dairy products the price should be reduced 
-// by 50% when the expiry date is less than 4 days into the future. For Canned goods 
-// the price should be reduced by 25% when the expiry date is less than 3 months into 
-// the future. For Vegetables the price should be reduced by 40% when the expiry date 
-// is less than 3 days into the future.
