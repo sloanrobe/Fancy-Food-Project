9142ba0ccc2d8f53f5f7642e1114bbbe76eb2832
Benji Sloan <sloanrobe@hotmail.com>
2025-03-16 23:04:23 +0100

    Added console.log statements to all if statements

--- src/PriceAdjuster.ts
+++ src/PriceAdjuster.ts
@@ -1,6 +1,3 @@
-// Purpose of this file is to implement functionality to reduce food item price
-// based on how close current date is to expiry date
-
 import { FoodItem } from "./FoodItem";
 
 export class PriceAdjuster {
@@ -17,15 +14,20 @@ export class PriceAdjuster {
 
             if (foodItem.category === 'Dairy' && this.shouldUpdatePrice(foodItem.expiry_date, new Date('2024-06-10'), 4)) {
                 foodItem.price *= 0.5
+                console.log(`The ${foodItem.item} has a reduced price of $${foodItem.price.toFixed(2)}.`)
             } else if (foodItem.category === 'Vegetables' && this.shouldUpdatePrice(foodItem.expiry_date, new Date('2024-06-10'), 3)) {
                 foodItem.price *= 0.6
+                console.log(`The ${foodItem.item} has a reduced price of $${foodItem.price.toFixed(2)}.`)
             } else if (foodItem.category === 'Canned Goods') {
                 const expiryDate = new Date(foodItem.expiry_date)
                 const threeMonthsFromNow = new Date(today)
                 threeMonthsFromNow.setMonth(today.getMonth() + 3)
                 if (expiryDate < threeMonthsFromNow) {
                     foodItem.price *= 0.75
+                    console.log(`The ${foodItem.item} has a reduced price of $${foodItem.price.toFixed(2)}.`)
                 } 
+            } else if ((foodItem.category === 'Dairy' || 'Canned Goods' || 'Vegetables') && foodItem.expiry_date  ) {
+                console.log (`The ${foodItem.item} has expired and needs to be thrown out.`)
             } else {
                 console.log('We do not recognize that food item category.')
             }
