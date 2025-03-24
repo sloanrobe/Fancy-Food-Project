6d1321c341a8a0e4ca0bde680ebf1e0561c8ad0e
Benji Sloan <sloanrobe@hotmail.com>
2025-03-24 15:03:38 +0100

    Minor edit

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,10 @@
 import { ExpirationAdjuster } from "./ExpirationAdjuster"
-import { PriceAdjuster } from "./PriceAdjuster";
-import { FoodItem } from "./FoodItem"
+import { PriceAdjuster } from "./PriceAdjuster"
 import { InventoryReader } from "./InventoryReader"
 import { QualityReader } from "./QualityReader"
 
 
-async function runQuality() {
+async function main() {
     const inventoryReader = new InventoryReader()  // this is an instantiation of InventoryReader (calling the constructor); inventoryReader is holding the instance
     const actualProducts = await inventoryReader.main()  // calling the main function from instance of the InventoryReader class
     
@@ -18,25 +17,7 @@ async function runQuality() {
     const myPriceAdjuster = new PriceAdjuster()
     myPriceAdjuster.checkPrice(adjustedProducts)
 
-    /* for (let i = 0; i < adjustedProducts.length; i++) {
-        const product = adjustedProducts[i]
-        const today = new Date(2024-6-10)
-        const expiryDate = new Date(product.expiry_date)
-
-        if (expiryDate < today) {
-            console.log(`The ${product} has expired and must be thrown out.`)
-        } else {
-            console.log(`The new reduced price of ${adjustedProducts[i]} is &{}`)
-        } 
-    } */
-    
-    //console.log("Adjusted Products After Expiration Adjustment:", adjustedProducts)
     return adjustedProducts
 }
 
-/* async function runPrice(adjustedProducts) {
-    const priceAdjuster = new PriceAdjuster()
-} */
-
-runQuality()
-//runPrice()
+main()
